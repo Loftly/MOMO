2fdda406a83af212782a9b745c406c94f584ca02
G G <gag1621@jagmail.southalabama.edu>
2023-09-20 10:00:01 -0500

    delete hamburger, static navbar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,33 +1,42 @@
-import { useState } from 'react';
-import Styles from './header.module.scss';
+import Styles from './header.module.scss'
 
 const headerOptions = [
-  //... your header options
-];
+    {
+        name: 'about',
+        title: 'About',
+        href: '#about'
+    },
+    {
+        name: 'tokenomics',
+        title: 'Tokenomics',
+        href: '#tokenomics'
+    },
+    {
+        name: 'roadmap',
+        title: 'Roadmap',
+        href: '#roadmap'
+    },
+    {
+        name: 'buy',
+        title: 'Buy',
+        className: Styles.link,
+        href: "https://app.uniswap.org/#/tokens/ethereum/0xbffd4d255f9917030b1dc8843ebdad531409c607",
+        target: '_blank'
+    }
+]
 
-export default function Header() {
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
-    return (
-        <div className={Styles.container}>
-            <div className={Styles.content}>
-                <div className={Styles.avatar}><a href='#'>MoeTheMartian</a></div>
-                <div className={Styles.menuIcon} onClick={toggleMenu}>
-                    ☰
-                </div>
-                <div className={`${Styles.navBar} ${menuOpen ? Styles.open : ''}`}>
-                    {headerOptions.map((ho, h) => ho.href 
-                        ? <a key={h} href={ho.href} target={ho.target}>
-                            <nav className={ho.className}>{ho.title}</nav>
-                        </a>
-                        : <nav key={h}>{ho.title}</nav>
-                    )}
-                </div>
+export default function Header(){
+    return <div className={Styles.container}>
+        <div className={Styles.content}>
+            <div className={Styles.avatar}><a href='#'>MoeTheMartian</a></div>
+            <div className={Styles.navBar}>
+                {headerOptions.map((ho, h) => ho.href 
+                    ? <a key={h} href={ho.href} target={ho.target}>
+                        <nav className={ho.className}>{ho.title}</nav>
+                    </a>
+                    : <nav key={h}>{ho.title}</nav>
+                )}
             </div>
         </div>
-    );
+    </div>
 }
